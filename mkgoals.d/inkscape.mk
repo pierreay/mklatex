5262a7b653e1c10c6b0513f69103d4841505917b
Pierre Ayoub <pierreay@pm.me>
2025-04-02 14:20:44 +0200

    style(inkscape): better style and logic

--- mkgoals.d/inkscape.mk
+++ mkgoals.d/inkscape.mk
@@ -2,33 +2,30 @@ include $(MKLATEX_PATH)/common.mk
 include $(MKLATEX_PATH)/mkrules.d/inkscape.mk
 
 .DEFAULT_GOAL := help
+.PHONY: all clean debug help
 
 # Export all figures.
-.PHONY: all
 all: $(INKSCAPE_BUILD_FILES)
 	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Inkscape build done!"
 
 # Clean exported figures.
-.PHONY: clean
 clean:
 	rm -fr $(INKSCAPE_BUILD_DIR)
 
 # Print module variables.
-.PHONY: clean
 debug:
 	@echo INKSCAPE_SRC_DIR=$(INKSCAPE_SRC_DIR)
 	@echo INKSCAPE_SRC_FILES=$(INKSCAPE_SRC_FILES)
 	@echo INKSCAPE_BUILD_DIR=$(INKSCAPE_BUILD_DIR)
 	@echo INKSCAPE_BUILD_FILES=$(INKSCAPE_BUILD_FILES)
 
-.PHONY: help
 help:
 	@echo -e "Usage: mklatex-inkscape [target] [variable]"
 	@echo -e ""
-	@echo -e "Targets:"
+	@echo -e "Goals:"
 	@echo -e "\tall\t\t\tBuild all Inkscape figures."
 	@echo -e "\tclean\t\t\tRemove all Inkscape figures."
 	@echo -e "\tdebug\t\t\tPrint Inkscape variables."
 	@echo -e ""
-	@echo -e "Rules:"
+	@echo -e "Targets:"
 	@echo -e "\t$(INKSCAPE_BUILD_DIR)/*.pdf\tBuild the PDF of the corresponding Inkscape figure."
--- mkrules.d/inkscape.mk
+++ mkrules.d/inkscape.mk
@@ -7,7 +7,7 @@ include $(MKLATEX_PATH)/common.mk
 INKSCAPE_SRC_DIR := $(MKLATEX_GFX_PATH)/inkscape
 
 # Source files.
-INKSCAPE_SRC_FILES := $(shell find $(INKSCAPE_SRC_DIR) -type f -name '*.svg')
+INKSCAPE_SRC_FILES := $(shell [ -d $(INKSCAPE_SRC_DIR) ] && find $(INKSCAPE_SRC_DIR) -type f -name '*.svg' || true)
 
 # Output filetype [pdf | eps].
 INKSCAPE_BUILD_FT := pdf
@@ -18,7 +18,7 @@ export INKSCAPE_BUILD_DIR := $(INKSCAPE_SRC_DIR:$(MKLATEX_SRC_DIR)/%=$(MKLATEX_B
 # Exported files (do not contains layered exportation).
 INKSCAPE_BUILD_FILES := $(patsubst $(INKSCAPE_SRC_DIR)/%.svg,$(INKSCAPE_BUILD_DIR)/%.$(INKSCAPE_BUILD_FT),$(INKSCAPE_SRC_FILES))
 
-# Custom exporter script.
+# Custom exporter script path.
 # TODO: As of now, must be cloned by top-level project as a separate submodule.
 # - May be included as a submodule of mklatex directly.
 # - Or may be indicated inside usage guide.
@@ -27,6 +27,11 @@ INKSCAPE_BUILD_FILES := $(patsubst $(INKSCAPE_SRC_DIR)/%.svg,$(INKSCAPE_BUILD_DI
 # Custom layers definitions.
 # TODO: Must be set by top-level project. Indicate it inside an usage guide.
 # INKSCAPE_LAYERS_PATH := inkscape2latex/examples/inkscape-layers.json
+
+# Custom exporter script arguments.
+ifdef INKSCAPE_LAYERS_PATH
+INKSCAPE_SCRIPT_ARGS += -l $(INKSCAPE_LAYERS_PATH)
+endif
 
 # Never delete the built files (even if the subsequent target fail).
 .PRECIOUS: $(INKSCAPE_BUILD_DIR)/%.$(INKSCAPE_BUILD_FT) $(INKSCAPE_BUILD_DIR)/%.$(INKSCAPE_BUILD_FT)_tex
@@ -48,8 +53,4 @@ $(INKSCAPE_BUILD_DIR):
 $(INKSCAPE_BUILD_DIR)/%.$(INKSCAPE_BUILD_FT): $(INKSCAPE_SRC_DIR)/%.svg | $(INKSCAPE_BUILD_DIR)
 	@[ -z "$(INKSCAPE_SCRIPT_PATH)" ] && \
 		{ echo -e "$(_COL_ERR)[x] mklatex:$(_COL_RES) INKSCAPE_SCRIPT_PATH variable not defined!"; exit 1; } || true
-ifndef INKSCAPE_LAYERS_PATH
-		$(INKSCAPE_SCRIPT_PATH) $< $@
-else
-		$(INKSCAPE_SCRIPT_PATH) -l $(INKSCAPE_LAYERS_PATH) $< $@
-endif
+		$(INKSCAPE_SCRIPT_PATH) $(INKSCAPE_SCRIPT_ARGS) $< $@
