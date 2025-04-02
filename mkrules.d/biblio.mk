8dac7c01c98fbae2574933832f33edac21e504b4
Pierre Ayoub <pierreay@pm.me>
2025-04-02 19:24:13 +0200

    refactor(biblio): overhaul and fix last issues

--- mkrules.d/biblio.mk
+++ mkrules.d/biblio.mk
@@ -3,41 +3,37 @@ include $(MKLATEX_PATH)/common.mk
 # Variables
 # ==============================================================================
 
-# Path to an external bibliography project.
-# TODO : To fix
-# BIB_REMOTE_FP = 
+# Path to a remote bibliography project.
+BIB_REMOTE_FP = /srv/annex/bibdocs
 
-BIB_REMOTE_CMD = $(MAKE) all
+# Command used to export the remote bibliography to local bibliography.
+BIB_REMOTE_CMD = $(MAKE) -C $(BIB_REMOTE_FP)
 
-# Test to find external bibliography [1 = found ; 0 = not found].
-BIB_REMOTE_FOUND := $(if $(shell test -d $(BIB_REMOTE_FP) && echo true),1,0)
-
-# Directory storing the local bibliographic files.
+# Local bibliography directory.
 # NOTE: Exported for `os.getenv` in `\directlua`.
 export BIB_DIR = $(MKLATEX_SRC_DIR)/bib
 
-# Files located in the external bibliography that we want to import.
-BIB_FILES = $(BIB_DIR)/references.bib $(BIB_DIR)/references-ownpubs.bib
+# Local bibliography files.
+BIB_FILES = $(BIB_DIR)/references.bib
 
 # Interface with other modules:
-# - Add bibliographic files as dependency for LaTeX document.
-# - Add update of external bibliography as order-only requirement for LaTeX document.
+# - Add local bibliography files as dependency for LaTeX document.
 LATEX_ADDITIONAL_DEPS += $(BIB_FILES)
-# TODO: bilio-make does not exists anymore (upgrade target)
-LATEX_ADDITIONAL_REQS += biblio-make
+
+# Empty target used in some prerequisite to force a target to re-run.
+# Useful force pattern rules that cannot be set directly into `.PHONY`.
+.PHONY: .FORCE_RERUN
+.FORCE_RERUN:
 
 # Targets
 # ==============================================================================
 
-# Remote .bib -> Local .bib
-# - cp -p: preserve timestamps.
-# - sed: remove field(s) that we don't want in our project.
-$(BIB_DIR)/%.bib: $(BIB_REMOTE_FP)/%.bib
-ifeq ($(BIB_REMOTE_FOUND), 1)
-	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Import bibliographic file from external project..."
-	cp -p $< $@
+# bibliography -> .bib
+# - Use `sed` to remove field(s) that we don't want in our project.
+# - Running condition is detected using the build system of the external
+#   bibliography, so it has to be forced run.
+$(BIB_DIR)/%.bib: .FORCE_RERUN
+	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Ensure bibliography is up-to-date..."
+	$(BIB_REMOTE_CMD) $$(realpath $@)
 	sed -i '/note.*=.*{.*}/d' $@
 	sed -i '/abstract.*=.*{.*}/d' $@
-else
-	$(warning [!] External bibliography not found!)
-endif
