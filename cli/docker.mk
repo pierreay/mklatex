1cbd660738a2519da4d7f3f2d96a4a1ed407b2da
Pierre Ayoub <pierreay@pm.me>
2025-04-07 20:53:42 +0200

    fix(docker): do not rm container when exiting

--- cli/docker.mk
+++ cli/docker.mk
@@ -12,12 +12,12 @@ include $(MKLATEX_PATH)/lib/docker.mk
 # does not handle it well. A better way would be to have a script that run the
 # appropriate command between Arch and Ubuntu.
 all: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
-	docker run $(MKLATEX_DOCKER_RUN_OPTS) --rm -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_TAG) /bin/bash -c "cd $(DOCKER_GWD) && make -j1 all"
+	docker run $(MKLATEX_DOCKER_RUN_OPTS) -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_TAG) /bin/bash -c "cd $(DOCKER_GWD) && make -j1 all"
 
 init: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
 
 test: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
-	docker run $(MKLATEX_DOCKER_RUN_OPTS) --rm -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_TAG) /bin/bash -c "cd $(DOCKER_GWD) && ls -alh"
+	docker run $(MKLATEX_DOCKER_RUN_OPTS) -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_TAG) /bin/bash -c "cd $(DOCKER_GWD) && ls -alh"
 
 shell: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
 	docker run $(MKLATEX_DOCKER_RUN_OPTS) -it -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_TAG) /bin/bash
