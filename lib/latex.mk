f9311e759067180fd2cb02638df388da67e72e25
Pierre Ayoub <pierreay@pm.me>
2025-04-09 08:18:01 +0200

    fix(lib/latex): missing delim for input command

--- lib/latex.mk
+++ lib/latex.mk
@@ -149,7 +149,7 @@ $(LATEX_BUILD_DIR)/%.pdf: $(LATEX_SRC_FILES) $(LATEX_ADDITIONAL_DEPS) | $(LATEX_
 	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Build $@..."
 ifndef LATEX_ONESHOT
 	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Run 1/3:"
-	cd $$(dirname $@) && $(LATEX_CC_BIN) $(LATEX_CC_ARGS_DRAFT) "$(LATEX_CC_FIRSTLINE)\input{$(patsubst $(LATEX_BUILD_DIR)/%.pdf,$(LATEX_SRC_DIR)/%,$@)"
+	cd $$(dirname $@) && $(LATEX_CC_BIN) $(LATEX_CC_ARGS_DRAFT) "$(LATEX_CC_FIRSTLINE)\input{$(patsubst $(LATEX_BUILD_DIR)/%.pdf,$(LATEX_SRC_DIR)/%,$@)}"
 	@grep -E -q $(LATEX_RERUNGLOSSARY_REGEX) $(patsubst %.pdf,%.log,$@) && $(MAKE) $(patsubst %.pdf,%.glg,$@) || true
 	@grep -E -q $(LATEX_RERUNBIB_REGEX)      $(patsubst %.pdf,%.log,$@) && $(MAKE) $(patsubst %.pdf,%.bbl,$@) || true
 endif
@@ -163,7 +163,7 @@ $(LATEX_BUILD_DIR)/%.glg: .FORCE_RERUN
 	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Glossary and index build..."
 	$(LATEX_GLOSSARY_CC_BIN) -d $$(dirname $@) $$(basename $(patsubst $(LATEX_BUILD_DIR)/%.glg,%,$@))
 	@echo -e "$(_COL_OK)[+] mklatex:$(_COL_RES) Run 2/3:"
-	cd $$(dirname $@) && $(LATEX_CC_BIN) $(LATEX_CC_ARGS_DRAFT) "$(LATEX_CC_FIRSTLINE)\input{$(patsubst $(LATEX_BUILD_DIR)/%.glg,$(LATEX_SRC_DIR)/%,$@)"
+	cd $$(dirname $@) && $(LATEX_CC_BIN) $(LATEX_CC_ARGS_DRAFT) "$(LATEX_CC_FIRSTLINE)\input{$(patsubst $(LATEX_BUILD_DIR)/%.glg,$(LATEX_SRC_DIR)/%,$@)}"
 
 # Bibliography build.
 # Running condition is detected using LaTeX logs by caller, so it has to be forced run.
