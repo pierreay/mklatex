513e84013fb748a6bd161acbcdb82d8e293deeb3
Pierre Ayoub <pierreay@pm.me>
2025-06-07 18:47:31 +0200

    refactor(docker): overhaul based on own external template

--- cli/docker.mk
+++ cli/docker.mk
@@ -1,3 +1,6 @@
+# Makefile inspired from:
+# Template: 4a045d95-227b-46ca-80b9-f279ce7ff162
+
 ifndef MKLATEX_PATH
 $(error Error: MKLATEX_PATH is not set!)
 endif
@@ -12,7 +15,7 @@ include $(MKLATEX_PATH)/lib/docker.mk
 # does not handle it well. A better way would be to have a script that run the
 # appropriate command between Arch and Ubuntu.
 all: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
-	docker run $(MKLATEX_DOCKER_RUN_OPTS) -v $(DOCKER_HWD):$(DOCKER_GWD) --name $(DOCKER_CONTAINER_NAME) -ai $(DOCKER_IMAGE_NAME) /bin/bash -c "cd $(DOCKER_GWD) && make -j1 all"
+	docker run $(MKLATEX_DOCKER_RUN_OPTS) -v $(DOCKER_HWD):$(DOCKER_GWD) -it $(DOCKER_IMAGE_NAME) /bin/bash -c "cd $(DOCKER_GWD) && make -j1 all"
 
 init: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
 
@@ -20,11 +23,18 @@ test: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
 	docker run $(MKLATEX_DOCKER_RUN_OPTS) -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_IMAGE_NAME) /bin/bash -c "cd $(DOCKER_GWD) && ls -alh"
 
 shell: $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
-	docker start -ai $(DOCKER_CONTAINER_NAME) || docker run $(MKLATEX_DOCKER_RUN_OPTS) -it -v $(DOCKER_HWD):$(DOCKER_GWD) $(DOCKER_IMAGE_NAME) /bin/bash
+	docker start -ai $(DOCKER_CONTAINER_NAME) 2>/dev/null || \
+		docker run $(MKLATEX_DOCKER_RUN_OPTS) -it \
+			--name $(DOCKER_CONTAINER_NAME) \
+			--env=DISPLAY --net=host \
+			-v $(DOCKER_HWD):$(DOCKER_GWD) \
+			$(DOCKER_IMAGE_NAME) /bin/bash
 
 # Clean our Docker images and containers.
 clean:
 	docker container ls --all | grep $(DOCKER_CONTAINER_NAME) | awk '{print $$1}' | xargs -n1 -r docker container rm
+
+distclean: clean
 	docker image rm $(DOCKER_IMAGE_NAME):latest
 	rm $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit
 
@@ -45,11 +55,12 @@ help:
 	@echo -e "Usage: mklatex-docker [target | goal] [variable...]"
 	@echo -e ""
 	@echo -e "Targets:"
-	@echo -e "\tall:\t\t\tBuild the project by running 'make all' inside the Docker container."
-	@echo -e "\tinit:\t\t\tInitialize the Docker container."
-	@echo -e "\ttest:\t\t\tTest such that Docker container is running and can access to our project."
-	@echo -e "\tshell:\t\t\tDrop a shell into the Docker container."
-	@echo -e "\tclean:\t\t\tClean this Docker image and container."
+	@echo -e "\tall:\t\t\tBuild the project by running 'make all' inside a new Docker container."
+	@echo -e "\tinit:\t\t\tInitialize a permanent Docker container."
+	@echo -e "\ttest:\t\t\tTest such that the permanent Docker container is running and can access to our project."
+	@echo -e "\tshell:\t\t\tDrop a shell into the permanent Docker container."
+	@echo -e "\tclean:\t\t\tClean our Docker containers."
+	@echo -e "\tdistclean\tClean our Docker containers and images."
 	@echo -e "\tprintenv\t\tPrint mklatex-docker variables."
 	@echo -e "\tprintexport\t\tPrint exported mklatex-docker variables."
 	@echo -e ""
--- lib/docker.mk
+++ lib/docker.mk
@@ -1,3 +1,6 @@
+# Makefile inspired from:
+# Template: 4a045d95-227b-46ca-80b9-f279ce7ff162
+
 include $(MKLATEX_PATH)/lib/common.mk
 
 # Variables
@@ -10,7 +13,7 @@ DOCKER_NAME ?= ubuntu
 # Name that will be use to name the Docker image.
 DOCKER_IMAGE_NAME := mklatex-docker/$(DOCKER_NAME)
 # Name that will be use to name the Docker the container (`/` are forbidden).
-DOCKER_CONTAINER_NAME := mklatex-docker@$(DOCKER_NAME)
+DOCKER_CONTAINER_NAME := mklatex-docker.$(DOCKER_NAME)
 
 # Working directory for the Docker host.
 # Use current working directory, since it is meant to be the full path of the
@@ -23,5 +26,5 @@ DOCKER_GWD = /latex
 # ==============================================================================
 
 $(DOCKER_ETC_PATH)/$(DOCKER_NAME)/.dockerinit:
-	docker build $(MKLATEX_DOCKER_BUILD_OPTS) -t $(DOCKER_TAG) $$(dirname $@)
+	docker buildx build $(MKLATEX_DOCKER_BUILD_OPTS) -t $(DOCKER_IMAGE_NAME) $$(dirname $@)
 	touch $@
