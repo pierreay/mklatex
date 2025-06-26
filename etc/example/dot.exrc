5149f0d33a303e0515c3e94cbf71a6443a77cf3b
Pierre Ayoub <pierreay@pm.me>
2025-06-26 14:56:54 +0200

    feat(example): use .exrc for (n)vim instead of .nvim.lua for nvim

---
+++ etc/example/dot.exrc
@@ -0,0 +1,2 @@
+" Allows (Neo)Vim to load MKLaTeX's `compiler/mklatex.vim` and `ftplugin/tex.vim` files.
+set runtimepath+=$MKLATEX_PATH/etc/vim
--- etc/example/dot.nvim.lua
+++
@@ -1,2 +0,0 @@
--- Allows NeoVim to load MKLaTeX's `compiler/mklatex.vim` and `ftplugin/tex.vim` files.
-vim.cmd[[set runtimepath+=$MKLATEX_PATH/etc/vim]]
