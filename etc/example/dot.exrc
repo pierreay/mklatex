fdc5ab27a4a6b4a1a9b369bedfe65e77ac8315f8
Pierre Ayoub <pierreay@pm.me>
2026-02-16 16:29:46 +0100

    docs(example): Add banner for copied files

--- etc/example/dot.exrc
+++ etc/example/dot.exrc
@@ -1,2 +1,6 @@
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"                           mklatex/.exrc                                      "
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
 " Allows (Neo)Vim to load MKLaTeX's `compiler/mklatex.vim` and `ftplugin/tex.vim` files.
 set runtimepath+=$MKLATEX_PATH/etc/vim
--- etc/example/etc/make/private.mk
+++ etc/example/etc/make/private.mk
@@ -1,3 +1,7 @@
+################################################################################
+#                           mklatex/private.mk                                 #
+################################################################################
+
 # You may defines how to generate your bibliography from an external project.
 # BIB_REMOTE_PATH = /path/to/my/biblio
 # BIB_REMOTE_CMD = $(MAKE) -C $(BIB_REMOTE_PATH)
